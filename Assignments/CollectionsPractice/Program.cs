95aef5a05523b80ccd63c86e267d79db913b67b7
ticarab2005 <ticarab2005@yahoo.com>
2021-11-29 05:26:14 -0600

    commit on main

---
+++ Assignments/CollectionsPractice/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[10] {0,1,2,3,4,5,6,7,8,9};
+            for(int i = 0; i < numArray.Length; i++)
+            {
+                Console.WriteLine(numArray[i]);
+            }
+            string[] strArray = new string[] {"Tim","Martin","Nikki","Sara"};
+            for(int i = 0; i < strArray.Length; i++)
+            {
+                Console.WriteLine(strArray[i]);
+            }
+            string[] arrayBool = new string[10] {$"true","false","true","false","true","false","true","false","true","false"};
+            for(int i = 0; i<arrayBool.Length; i++)
+            {
+                Console.WriteLine(arrayBool[i]);
+            }
+            List<string> names = new List<string>();
+            names.Add("Strawberry");
+            names.Add("Mint");
+            names.Add("Vanilla");
+            names.Add("Chocolate");
+            names.Add("Sherbert");
+            {
+                Console.WriteLine(String.Join(", ", names));
+            }
+                Console.WriteLine(names[3]);
+            names.Remove("Chocolate");
+            {
+                Console.WriteLine(String.Join(", ", names));
+            }
+            Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+            myDictionary.Add("Tim","Strawberry");
+            myDictionary.Add("Martin","Mint");
+            myDictionary.Add("Nikki","Vanilla");
+            myDictionary.Add("Sara","Sherbert");
+            foreach(KeyValuePair<string,string> val in myDictionary)
+            {
+                Console.WriteLine(val.Key + " : " + val.Value );
+            }
+
+
+
+
+
+        }
+    }
+}
--- Lectures/FundamentalsDemo/Program.cs
+++ Lectures/FundamentalsDemo/Program.cs
@@ -1,39 +1,68 @@
-﻿// using System;
+﻿using System;
+using System.Collections.Generic;
 
-// namespace FundamentalsDemo
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             Console.WriteLine("Fundamentals demo!");
-//             // C# does accept a var datatype
-//             var something = "something";
-//             // We want to be explicit with our variable types
-//             // String
-//             string something2 = "something";  
-//             //
-//             Console.WriteLine(something);
-//         }
-//     }
-// }
-
-using System;
-
-public class Program
+namespace FundamentalsDemo
 {
-    public static void Main()
+    class Program
     {
-        int sum = 0;
-        for (int i = 0; 3 * i < 1000; i++)
+        static void Main(string[] args)
         {
-            sum += 3 * i;
-
-            if (5 * i < 1000 && (5 * i) % 3 != 0)
+            Console.WriteLine("Fundamentals demo!");
+            // C# does accept a var datatype
+            // var something = "something";
+            // We want to be explicit with our variable types
+            // String
+            // string something2 = "something";  
+            // //Integer
+            // int num = 7;
+            // // Boolean
+            // bool fact = true;
+            // // Array
+            // [1,2,3,4,5]
+            // ["hello","world"]
+            // ["one", 2 ,"world"]
+            // Arrays MUST  have a predefined number of values stored in it
+            int[] numberArray = new int[] {1,2,3,4,5,7,6,10};
+            numberArray[3] = 17;
+            Console.WriteLine(numberArray);
+            for(int i = 0; i <numberArray.Length; i++)
             {
-                sum += 5 * i;
+                Console.WriteLine(numberArray[i]);
             }
+            // List
+            List<string> names = new List<string>();
+            names.Add("7");
+            names.Add("Nichole");
+            names.Add("Mitch");
+            names.Add("7");
+            names.Add("Ryan");
+            // Removes the first instance it encounters that matches the search query
+            names.Remove("7");
+            names.Remove("7");
+            // names.RemoveAt(2);
+            names.Insert(1, "Habib");
+            // Console.WriteLine(names[1]);
+            foreach(string name in names)
+            {
+                Console.WriteLine($"{name}");
+            }
+            Console.WriteLine(String.Join(", ", names));
+            // Dictionary
+            // A dictionary is made up of keys and values
+            // {"key": "value", "key2",: "value2" }
+            Dictionary<string,int> myDictionary = new Dictionary<string, int>();
+            myDictionary.Add("A", 10);
+            myDictionary.Add("B", 25);
+            Console.WriteLine($"The value at A is: {myDictionary["A"]}");
+            foreach(KeyValuePair<string,int> val in myDictionary)
+            {
+                Console.WriteLine(val.Key + ":" + val.Value );
+            }
+            foreach(var val in myDictionary)
+            {
+                Console.WriteLine(val.Key + ":" + val.Value );
+            }
+            // Console.WriteLine(something);
         }
-        Console.WriteLine(sum );
     }
 }
