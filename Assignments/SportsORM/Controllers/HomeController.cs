72a0c0af09f06fe249c9a21f5e839ad971b27861
Ticara Bell <ticarab2005@yahoo.com>
2021-12-08 05:42:21 -0600

    commit

--- Assignments/SportsORM/Controllers/HomeController.cs
+++ Assignments/SportsORM/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using SportsORM.Models;
 
 
@@ -31,6 +32,22 @@ namespace SportsORM.Controllers
         [HttpGet("level_1")]
         public IActionResult Level1()
         {
+            ViewBag.WomensLeague = _context.Leagues.Where(w => w.Name.Contains("Womens'"));
+            ViewBag.HockeyLeague = _context.Leagues.Where(h => h.Sport.Contains("Hockey"));
+            ViewBag.NoFootballLeague = _context.Leagues.Where(nf => nf.Sport != "Football");
+            ViewBag.ConferenceLeague = _context.Leagues.Where(c => c.Name.Contains("Conference"));
+            ViewBag.AtlanticLeague = _context.Leagues.Where(a => a.Name.Contains("Atlantic"));
+            ViewBag.DallasTeams = _context.Teams.Where(d => d.Location == "Dallas");
+            ViewBag.RaptorsTeams = _context.Teams.Where(r => r.TeamName.Contains("Raptors"));
+            ViewBag.CityTeams = _context.Teams.Where(ct => ct.Location.Contains("City"));
+            ViewBag.TTeams = _context.Teams.Where(t => t.TeamName.Contains("T"));
+            ViewBag.TopALocationTeams = _context.Teams.OrderBy(tl => tl.Location);
+            ViewBag.BottomATeams = _context.Teams.OrderByDescending(bt => bt.TeamName).ToList();
+            
+            ViewBag.NameCooper = _context.Players.Where(ln => ln.LastName == "Cooper");
+            ViewBag.NameJoshua = _context.Players.Where(fn => fn.FirstName == "Joshua");
+            ViewBag.ExceptJoshua = _context.Players.Where(ex => ex.LastName.Contains("Cooper") && !ex.FirstName.Contains("Joshua"));
+            ViewBag.EitherOr = _context.Players.Where(eo => eo.FirstName.Contains("Alexander") || eo.FirstName.Contains("Wyatt"));
             return View();
         }
 
--- Lectures/LinqDemo/Controllers/HomeController.cs
+++ Lectures/LinqDemo/Controllers/HomeController.cs
@@ -22,13 +22,13 @@ namespace LinqDemo.Controllers
         {
             Game[] allGames = new Game[]
             {
-                new Game{Title="Pokemon Brilliant Diamon", Genre="Adventure",Price=60.00,Rating="E",Platform="Nintento Switch",minNumPlayers=1,maxNumPlayers=4},
-                new Game{Title="Plasmophobia", Genre="Horror",Price=13.99,Rating="M",Platform="PC",minNumPlayers=1,maxNumPlayers=4},
-                new Game{Title="Modern Warfare 3", Genre="First Person Shooter",Price=59.99,Rating="E",Platform="Nintento Switch",minNumPlayers=1,maxNumPlayers=1},
-                new Game{Title="Pokemon Brilliant Diamon", Genre="Adventure",Price=60.00,Rating="E",Platform="Nintento Switch",minNumPlayers=1,maxNumPlayers=4},
-                new Game{Title="Pokemon Brilliant Diamon", Genre="Adventure",Price=60.00,Rating="E",Platform="Nintento Switch",minNumPlayers=1,maxNumPlayers=4},
-                new Game{Title="Pokemon Brilliant Diamon", Genre="Adventure",Price=60.00,Rating="E",Platform="Nintento Switch",minNumPlayers=1,maxNumPlayers=4},
-                new Game{Title="Dreamland", Genre="Adventure",Price=25.00,Rating="E",Platform="Nintento Switch",minNumPlayers=1,maxNumPlayers=4},
+                new Game {Title = "Pokemon Brilliant Diamond", Genre = "Adventure", Price = 60.00, Rating = "E", Platform = "Nintendo Switch", minNumPlayers = 1, maxNumPlayers = 1},
+                new Game {Title = "Phasmophobia", Genre = "Horror", Price = 19.99, Rating = "M", Platform = "PC", minNumPlayers = 1, maxNumPlayers = 4},
+                new Game {Title = "Death's Door", Genre = "Action", Price = 19.99, Rating = "T", Platform = "PC", minNumPlayers = 1, maxNumPlayers = 1},
+                new Game {Title = "Life Is Strange", Genre = "Adventure", Price = 0.00, Rating = "M", Platform = "PC", minNumPlayers = 1, maxNumPlayers = 1},
+                new Game {Title = "Halo Infinite", Genre = "FPS", Price = 59.99, Rating = "M", Platform = "XBOX", minNumPlayers = 1, maxNumPlayers = 24},
+                new Game {Title = "The Legend of Zelda: Ocarina of Time", Genre = "Adventure", Price = 1000000.00, Rating = "E", Platform = "Nintendo 64", minNumPlayers = 1, maxNumPlayers = 1},
+                new Game {Title = "Dreamlands", Genre = "Adventure", Price = 25.00, Rating = "E", Platform = "Role Playing", minNumPlayers = 1, maxNumPlayers = 4}
             };
 
             // This line is a for each loop
@@ -51,14 +51,14 @@ namespace LinqDemo.Controllers
                 // Pass into List
             // }
             double priceMax = 20.00;
-            // List<Game> AdventureGames = allGames.Where(adventG => adventG.Genre =="Adventure").OrderByDescending(n => n.Title).ToList();
             string genre = "Adventure";
-            List<Game> AdventureGames = allGames.Where(adventG => adventG.Genre ==genre).OrderByDescending(n => n.Title).ToList();
-            // List<Game> AdventureGames = allGames.Where(adventG => adventG.Genre =="Adventure").ToList();
             ViewBag.AllGames = allGames;
+            List<Game> AdventureGames = allGames.Where(adventG => adventG.Genre == genre).OrderByDescending(n => n.Title).ToList();
+            // This is the top level version
+            // IEnumerable<Game> AdventureGames = allGames.Where(adventG => adventG.Genre == "Adventure");
             ViewBag.AdventGames = AdventureGames;
-            ViewBag.MaxPlayers = allGames.OrderBy(a => a.maxNumPlayers).ToList();
-            ViewBag.Under20 = allGames.Where(g=>g.Price < priceMax).ToList();
+            ViewBag.MaxPlayers = allGames.OrderBy(a => a.maxNumPlayers);
+            ViewBag.Under20 = allGames.Where(g => g.Price < priceMax).ToList();
             return View();
         }
 
